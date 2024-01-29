e97a2b588e3311317e762a48af337e37854b75e4
smojescikdev <s.mojescik.dev@gmail.com>
2024-01-29 16:53:30 +0100

    DBUtil Class with connection to DB - added User and UserDau classes - added without methods.

---
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -0,0 +1,16 @@
+package pl.coderslab;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "Start12345";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+}
+
